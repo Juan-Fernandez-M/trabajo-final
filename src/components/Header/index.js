a27ebff602f9584728c7272a4b410b7b75cad819
Juan <tinofernandez97@gmail.com>
2023-11-01 09:03:43 -0300

    segundo commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import SearchForm from './Search';
 const Header = () => {
     return (
         <header className="header-movile">
-            <img className="logo" src={logo} alt="Logo" />
+            <img className="logo" src="../keepoff/public/images/62CC4021-5D65-4C61-A433-F75DF670E05A.png" alt="logo" />
             <ul className="menu-header">
                 <li><Link to="/" className='links'>About us</Link></li>
                 <li><Link to="/products" className='links'>Products</Link></li>
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -8,22 +8,12 @@ const Main = () => {
         </section>
         <div className="banner">
             
-            <p>#TelorecomiendoKiptonitaGrow</p>
+            <p>#bediferent</p>
         </div>
-        <section className="nosotros">
+        <section className="us">
         <div>
-            <h4 id="nosotros">Sobre nosotros</h4>
-            <p>Somos una empresa nueva en el mercado del growshop y estamos comprometidos en ofrecer productos de alta calidad para el cultivo de plantas en casa . Nuestro objetivo es ayudar a nuestros clientes a obtener los mejores resultados en su cultivo, brindando asesoramiento y recomendaciones personalizadas. Estamos comprometidos en ofrecer un servicio excepcional y en mantenernos actualizados con las últimas tendencias y tecnologías en el mercado del growshop</p>
+            <h4 id="nosotros">About us</h4>
             </div>
-        <div className="elergirnos">
-            <h2>¿Por qué elegirnos?</h2>
-            <ol>
-                <li>Nuestro personal está altamente capacitado y siempre está dispuesto a ayudarte con cualquier pregunta que puedas tener. </li>
-                <li>Ofrecemos una amplia variedad de productos para satisfacer las necesidades de cualquier cultivador, desde principiantes hasta expertos. </li>
-                <li>Te ayudamos a considerar las necesidades específicas de las plantas que se desean cultivar, como la cantidad de luz, agua y nutrientes que requieren.</li>
-            </ol>
-            <p><i>¡No dudes en visitarnos y comenzar tu viaje en el mundo del cultivo hoy mismo!</i></p>
-        </div>
         </section>
         </div>
     );
